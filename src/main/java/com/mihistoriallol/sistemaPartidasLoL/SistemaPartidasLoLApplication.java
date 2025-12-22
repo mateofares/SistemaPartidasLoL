5de7a35be84a230438b1e2cfd63cb80e329964d1
[Mateo Fares] <faresmateo2005@gmail.com>
2025-12-22 15:24:08 -0300

    initial spring boot project

---
+++ src/main/java/com/mihistoriallol/sistemaPartidasLoL/SistemaPartidasLoLApplication.java
@@ -0,0 +1,13 @@
+package com.mihistoriallol.sistemaPartidasLoL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaPartidasLoLApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaPartidasLoLApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mihistoriallol/sistemaPartidasLoL/SistemaPartidasLoLApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mihistoriallol.sistemaPartidasLoL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaPartidasLoLApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
